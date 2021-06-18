77e49939f2980ae3bb8fbf20630677d081790bdc
Lina Huertas <linamarcelahuertas7@gmail.com>
2021-06-17 20:40:42 -0500

    Se agregan los archivos correspondientes al proyecto

---
+++ js/listado1.js
@@ -0,0 +1,51 @@
+const listado1 = [
+    {
+        enlace: 'Mujer',
+        title: 'Tendencia',
+        imgUrl: './img/1.png',
+        enlace2: 'Comprar'
+    },
+    {
+        enlace: 'Mujer',
+        title: 'Tendencia',
+        imgUrl: './img/2.png',
+        enlace2: 'Comprar'
+    },
+    {
+        enlace: 'Mujer',
+        title: 'Tendencia',
+        imgUrl: './img/1.png',
+        enlace2: 'Comprar'
+    },
+    {
+        enlace: 'Mujer',
+        title: 'Tendencia',
+        imgUrl: './img/2.png',
+        enlace2: 'Comprar'
+    }
+];
+
+const listado2 = [
+    {
+        imgUrl: './img/remera.png',
+        title: 'Remerita linda',
+        precio: '$100.00'
+    },
+    {
+        imgUrl: './img/remera.png',
+        title: 'Remerita linda',
+        precio: '$100.00'
+    },
+    {
+        imgUrl: './img/remera.png',
+        title: 'Remerita linda',
+        precio: '$100.00'
+    },
+    {
+        imgUrl: './img/remera.png',
+        title: 'Remerita linda',
+        precio: '$100.00'
+    }
+];
+
+
