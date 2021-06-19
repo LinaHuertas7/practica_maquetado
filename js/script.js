0192da77b6157d04a3af8786eaa283c8d921fcc7
Lina Huertas <linamarcelahuertas7@gmail.com>
2021-06-19 14:53:58 -0500

    Se agregaron actualizaciones al proyecto

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@ const contenedor = document.querySelector('.segunda-parte')
 const contenedor2 = document.querySelector('.contenedor-remera')
 
 listado1.forEach(mujer1 => {
-    contenedor.innerHTML += `<div class="imagen" style="background-image: url('${mujer1.imgUrl}');">
+    contenedor.innerHTML += `
+    <div class="imagen" style="background-image: url('${mujer1.imgUrl}');">
     <a class="enlace1" href="">${mujer1.enlace}</a>
     <h2>${mujer1.title}</h2>
-    <a class ="enlace2" href="">${mujer1.enlace2}</a>
-        
-</div>`
-}); 
+    <a class ="enlace2" href="">${mujer1.enlace2}</a> 
+    </div>
+    `
+});
 
 listado2.forEach(remera => {
-    contenedor2.innerHTML += `<div class="remera">
+    contenedor2.innerHTML += `
+    <div class="remera">
     <img src='${remera.imgUrl}'>
     <h2>${remera.title}</h2>
     <p>${remera.precio}</p>
     </div>
-`  
+`
 });
-
-
 
